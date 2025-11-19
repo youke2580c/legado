dc8e37a2c11ecd9183f839e950d9179b6333e03e
Luoyacheng <1816669175@qq.com>
2025-11-20 03:17:09 +0800

    [功能]：内置浏览器支持更多异步java函数

--- app/src/main/java/io/legado/app/help/WebJsExtensions.kt
+++ app/src/main/java/io/legado/app/help/WebJsExtensions.kt
@@ -56,6 +56,30 @@ class WebJsExtensions(private val source: BaseSource, private val activity: AppC
                 "webViewAwait" -> {
                     webView(jsParam[0], jsParam[1], jsParam[2]).toString()
                 }
+                "decryptStrAwait" -> {
+                    createSymmetricCrypto(jsParam[0], jsParam[1], jsParam[2]).decryptStr(jsParam[3])
+                }
+                "encryptBase64Await" -> {
+                    createSymmetricCrypto(jsParam[0], jsParam[1], jsParam[2]).encryptBase64(jsParam[3])
+                }
+                "encryptHexAwait" -> {
+                    createSymmetricCrypto(jsParam[0], jsParam[1], jsParam[2]).encryptHex(jsParam[3])
+                }
+                "createSignHexAwait" -> {
+                    createSign(jsParam[0]).setPublicKey(jsParam[1]).setPrivateKey(jsParam[2]).signHex(jsParam[3])
+                }
+                "downloadFileAwait" -> {
+                    downloadFile(jsParam[0])
+                }
+                "readTxtFileAwait" -> {
+                    readTxtFile(jsParam[0])
+                }
+                "importScriptAwait" -> {
+                    importScript(jsParam[0])
+                }
+                "getStringAwait" -> {
+                    AnalyzeRule(source = source).getString(jsParam[0], jsParam[1])
+                }
                 else -> "error funName"
             }
         }.onSuccess { data ->
@@ -185,6 +209,70 @@ class WebJsExtensions(private val source: BaseSource, private val activity: AppC
                     window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
                     window.JSBridgeCallbacks[id] = { resolve, reject };
                     window.java?.request("webViewAwait", [String(html), String(url), String(js)], id);
+                });
+            };
+            window.decryptStrAwait = function(transformation, key, iv, data) {
+                return new Promise((resolve, reject) => {
+                    const id = requestId("decryptStrAwait");
+                    window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
+                    window.JSBridgeCallbacks[id] = { resolve, reject };
+                    window.java?.request("decryptStrAwait", [String(transformation), String(key), String(iv), String(data)], id);
+                });
+            };
+            window.encryptBase64Await = function(transformation, key, iv, data) {
+                return new Promise((resolve, reject) => {
+                    const id = requestId("encryptBase64Await");
+                    window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
+                    window.JSBridgeCallbacks[id] = { resolve, reject };
+                    window.java?.request("encryptBase64Await", [String(transformation), String(key), String(iv), String(data)], id);
+                });
+            };
+            window.encryptHexAwait = function(transformation, key, iv, data) {
+                return new Promise((resolve, reject) => {
+                    const id = requestId("encryptHexAwait");
+                    window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
+                    window.JSBridgeCallbacks[id] = { resolve, reject };
+                    window.java?.request("encryptHexAwait", [String(transformation), String(key), String(iv), String(data)], id);
+                });
+            };
+            window.createSignHexAwait = function(algorithm, publicKey, privateKey, data) {
+                return new Promise((resolve, reject) => {
+                    const id = requestId("createSignHexAwait");
+                    window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
+                    window.JSBridgeCallbacks[id] = { resolve, reject };
+                    window.java?.request("createSignHexAwait", [String(algorithm), String(publicKey), String(privateKey), String(data)], id);
+                });
+            };
+            window.downloadFileAwait = function(url) {
+                return new Promise((resolve, reject) => {
+                    const id = requestId("downloadFileAwait");
+                    window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
+                    window.JSBridgeCallbacks[id] = { resolve, reject };
+                    window.java?.request("downloadFileAwait", [String(url)], id);
+                });
+            };
+            window.readTxtFileAwait = function(path) {
+                return new Promise((resolve, reject) => {
+                    const id = requestId("readTxtFileAwait");
+                    window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
+                    window.JSBridgeCallbacks[id] = { resolve, reject };
+                    window.java?.request("readTxtFileAwait", [String(path)], id);
+                });
+            };
+            window.importScriptAwait = function(url) {
+                return new Promise((resolve, reject) => {
+                    const id = requestId("importScriptAwait");
+                    window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
+                    window.JSBridgeCallbacks[id] = { resolve, reject };
+                    window.java?.request("importScriptAwait", [String(url)], id);
+                });
+            };
+            window.getStringAwait = function(ruleStr, mContent) {
+                return new Promise((resolve, reject) => {
+                    const id = requestId("getStringAwait");
+                    window.JSBridgeCallbacks = window.JSBridgeCallbacks || {};
+                    window.JSBridgeCallbacks[id] = { resolve, reject };
+                    window.java?.request("getStringAwait", [String(ruleStr), String(mContent)], id);
                 });
             };
             window.JSBridgeResult = function(requestId, result, error) {
